7a0193bd3169bd9f90065282c636c460881eb87f
Nick <nick.bucheleres@gmail.com>
2015-05-08 12:30:56 -0700

    full post data shows up on single_post page

--- client/new_post/new_post.js
+++ client/new_post/new_post.js
@@ -15,9 +15,11 @@ Template.new_post_template.events({
 		var new_post_fields = {
 			post_title_field : template.$('#new_post_input').val(),
 			post_text_field : template.$('#new_post_textarea').val(),
+			post_value_field : template.$('#new_post_value').val(),
 			tag_1: template.$('#select_one').val(),
 			tag_2: template.$('#select_two').val(),
 			tag_3: template.$('#select_three').val(),
+			needed_by_field: template.$('#completion_date').val(),
 			created: today.toDateString()
 		}
 
