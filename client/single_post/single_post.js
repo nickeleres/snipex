8795e1dbcb93b34f36261d17f0d01ea582f7511f
Nick <nick.bucheleres@gmail.com>
2015-05-10 11:56:24 -0700

    deleting post and messages now works

--- client/single_post/single_post.js
+++ client/single_post/single_post.js
@@ -4,24 +4,46 @@ Router.route('single_post', {
 	path: '/post/:_id',
 	layoutTemplate: 'layoutTemplate',
 	template: 'single_post_template',
+	onBeforeAction: function(){
+		Session.set('post_id', this.params._id);
+		console.log(Session.get('post_id'));
+		this.next();
+	},
+
 	waitOn: function(){
-		Session.set('id', this.params._id);
 		return [
 			Meteor.subscribe('posts'),
-			Meteor.subscribe('messages', Session.get('id'))
+			Meteor.subscribe('messages', Session.get('post_id'))
 		]
 	}
 });
 
 Template.single_post_template.helpers({
 	posts: function(){
-		return postsCollection.find({_id: Session.get('id')});
+		return postsCollection.find({_id: Session.get('post_id')});
 	}
+});
+
+Template.single_post.helpers({
+	isOwner: function(){
+		var current_post = postsCollection.find({_id: Session.get('post_id')}).fetch();
+		var current_owner = current_post[0].owner;
+		return current_owner == Meteor.userId();
+	}
+});
+
+Template.single_post.events({
+	'click #delete_post_button': function(ev){
+		ev.preventDefault();
+		Meteor.call('delete_post_and_messages', Session.get('post_id'));
+		this.render('home');
+	}
+	
 });
 
 Template.single_post_messages.helpers({
 	messages: function(){
-		return messagesCollection.find({post_id: Session.get('id')});
+		return messagesCollection.find({post_id: Session.get('post_id')});
 	}
 })
 
@@ -34,7 +56,7 @@ Template.single_post_template.events({
 		var new_message_data = {
 			body: template.$('#message_textarea').val(),
 			poster: Meteor.userId(),
-			post_id: Session.get('id'),
+			post_id: Session.get('post_id'),
 			created: today.toDateString()
 		}
 
@@ -42,5 +64,7 @@ Template.single_post_template.events({
 
 		Meteor.call('addMessage', new_message_data);
 	}
-})
+});
+
+
 
--- server/publications/posts.js
+++ server/publications/posts.js
@@ -1,9 +1,5 @@
 //server/publications/posts.js
 
 Meteor.publish('posts', function(){
-	if(!this.userId){
-		this.ready();	
-	} else {
-		return postsCollection.find();
-	}
+	return postsCollection.find();
 });
