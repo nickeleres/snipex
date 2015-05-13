78b478b72fbaa74dd6dd06b92983b441a53bd4f7
Nick <nick.bucheleres@gmail.com>
2015-05-13 13:12:13 -0700

    winning bid shows verified status

--- client/verified/verified.js
+++ client/verified/verified.js
@@ -3,5 +3,15 @@
 Router.route('verified', {
 	path: '/verified',
 	layoutTemplate: 'layoutTemplate',
-	template: 'verifiedTemplate'
+	template: 'verifiedTemplate',
+	waitOn: function(){
+		return [
+			Meteor.subscribe('posts'),
+			Meteor.subscribe('messages', Session.get('post_id')),
+			Meteor.subscribe('users'),
+			Meteor.subscribe('contractorMatches', Meteor.userId()),
+			Meteor.subscribe('posterMatches', Meteor.userId()),
+			Meteor.subscribe('matches', Session.get('post_id'))
+		]
+	}
 });
